192b13d13c5a8c0448f4e5f22d123e52b167cd18
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2024-09-19 10:46:56 -0700

    Fix errors

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -12,12 +12,11 @@ public class ElevatorConstants {
 
     public static final double ANGLE = 0; // radians, from vertical
     public static final DCMotor MOTOR = DCMotor.getKrakenX60Foc(1);
+    public static final int NUMBER_OF_STAGES = 3;
     public static final double GEARING = 18 * NUMBER_OF_STAGES;
     public static final double MIN_HEIGHT = 0; // meters
     public static final double MAX_HEIGHT = 4; // meters
     public static final double START_HEIGHT = MIN_HEIGHT; // meters
     public static final double CARRIAGE_MASS = 10; // kilograms
     public static final double DRUM_RADIUS = 0.0325374/2; // meters
-    
-    public static final int NUMBER_OF_STAGES = 3
 }
