85eef065e8aab39fc66a2613d23a8b6308feb658
GLRoylance <GLRoylance@gmail.com>
2024-10-24 17:21:19 -0700

    PowerPanel unit tests

---
+++ src/test/java/frc/robot/subsystems/PowerPanelTest.java
@@ -0,0 +1,207 @@
+package frc.robot.subsystems;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.simulation.BatterySim;
+import edu.wpi.first.wpilibj.simulation.PDPSim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
+
+/**
+ * WPILib examples of battery voltage simulation are too simplified.
+ * Sort out some issues with simulation and unit tests.
+ * <p>
+ * The BatterySim class works.
+ * <p>
+ * The PowerDistribution class work for both the PDP and the PDH.
+ * <p>
+ * The RoboRioSim class fails completely.
+ * <p>
+ * The RobotController class fails but some methods work.
+ * <p>
+ * Looks like some code may work with Simulation but not Unit Tests.
+ */
+public class PowerPanelTest {
+
+    /**
+     * The BatterySim class just computes Ohm's law via static methods.
+     * The 20 milohm value should be modified to include wire and breaker resistances leading to the PowerPanel.
+     * For example, 4 feet of 6 AWG wire (0.6 milohms/foot) has a resistance of 2.4 milohms.
+     * The 120 A breaker has a resistance of 0.6 milohms.
+     * The Anderson SB50 connector is not clear. Maybe 0.2 milohms each for 0.4 milohms.
+     */
+    @Test
+    public void batterySimTest() {
+        // BatterySim just does some Ohm's law calculations
+        // BatterySim is a final class with static methods
+        // BatterySim.calculateLoadedBatteryVoltage(V, R, I)
+
+        // default battery resistance is 20 milohms
+        double R = 0.020;
+        // default battery voltage is 12 volts
+        double V = 12.0;
+        // set the current draw
+        double I = 100.0;
+
+        // both methods should give the same answer
+        assertEquals(
+            BatterySim.calculateDefaultBatteryLoadedVoltage(I),
+            BatterySim.calculateLoadedBatteryVoltage(V, R, I),
+            0.001);
+
+        // the voltage answer should follow Ohm's law
+        assertEquals(V - I * R, BatterySim.calculateLoadedBatteryVoltage(V, R, I), 0.001);
+    }
+
+    /**
+     * Test the PDP simulator object.
+     * Values set by the simulator immediately change the underlying PowerDistributionn object.
+     */
+    @Test
+    public void pdpSimTest() {
+        // make a CTRE Power Distribution Panel
+        //    could do new PowerDistribution(0, ModuleType.kCTRE);
+        PowerDistribution pdp = new PowerDistribution();
+
+        // make its simulator
+        PDPSim pdpSim = new PDPSim(pdp);
+
+        // at the start, the voltage should be 12 volts
+        assertEquals(12.0, pdp.getVoltage(), 0.001);
+
+        // set a different voltage
+        pdpSim.setVoltage(11.0);
+
+        // check the new voltage
+        assertEquals(11.0, pdp.getVoltage(), 0.001);
+
+        // set some channel currents
+        pdpSim.setCurrent(1, 10.0);
+        pdpSim.setCurrent(2, 15.0);
+
+        // check the individual currents
+        assertEquals(10.0, pdp.getCurrent(1), 0.001);
+        assertEquals(15.0, pdp.getCurrent(2), 0.001);
+
+        // check the total current
+        assertEquals(25.0, pdp.getTotalCurrent(), 0.001);
+
+        // close the PDP
+        pdp.close();
+    }
+
+    @Test
+    public void pdhSimTest() {
+        // make a REV Power Distribution Hub
+        PowerDistribution pdp = new PowerDistribution(1, ModuleType.kRev);
+
+        // make its simulator
+        PDPSim pdpSim = new PDPSim(pdp);
+
+        // at the start, the voltage should be 12 volts
+        assertEquals(12.0, pdp.getVoltage(), 0.001);
+
+        // set a different voltage
+        pdpSim.setVoltage(11.0);
+
+        // check the new voltage
+        assertEquals(11.0, pdp.getVoltage(), 0.001);
+
+        // set some channel currents
+        pdpSim.setCurrent(1, 10.0);
+        pdpSim.setCurrent(2, 15.0);
+
+        // check the individual currents
+        assertEquals(10.0, pdp.getCurrent(1), 0.001);
+        assertEquals(15.0, pdp.getCurrent(2), 0.001);
+
+        // check the total current
+        assertEquals(25.0, pdp.getTotalCurrent(), 0.001);
+
+        // close the PDP
+        pdp.close();
+    }
+
+    // TODO: RoboRioSim.getVInVoltage() FAILS!
+    @Disabled
+    @Test
+    public void roboRioSimTest() {
+        // RoboRioSim is a final class with static methods
+        // https://github.wpilib.org/allwpilib/docs/release/java/edu/wpi/first/wpilibj/simulation/RoboRioSim.html
+
+        // trying to set the voltage first does not help...
+        // EXCEPTION_ACCESS_VIOLATION (0xc0000005) at pc=0x00007ffab7bc4198, pid=27672, tid=13480
+        // Unexpected exception thrown.
+        // org.gradle.internal.remote.internal.MessageIOException: Could not write '/127.0.0.1:53566'.
+        // Possible explanation: the method wants the Driver Station to display the simulated voltage.
+        RoboRioSim.setVInVoltage(12.0);
+
+        // another place to get the battery voltage
+        // Unexpected exception thrown
+        assertEquals(12.0, RoboRioSim.getVInVoltage(), 0.01);
+    }
+
+    @Test
+    public void roboRioSimMethodsTest() {
+        // These results suggest the GUI simulator must be running
+
+        // There is a RoboRioSim.setSerialNumber()
+        // fails: access violation
+        // RoboRioSim.setSerialNumber("12345");
+        // fails: could not write /127.0.0.1:55494.
+        // assertEquals("12345", RoboRioSim.getSerialNumber());
+
+        // fails: could not write /127.0.0.1:55609.
+        // RoboRioSim.getTeamNumber();
+    }
+ 
+    // TODO: RobotController.getBatteryVoltage() FAILS!
+    @Disabled
+    @Test
+    public void robotControllerTest() {
+        // RobotController is final class with static methods.
+        // https://github.wpilib.org/allwpilib/docs/release/java/edu/wpi/first/wpilibj/RobotController.html
+
+        // one place is to get the battery voltage from RobotController
+        // Produces EXCEPTION_ACCESS_VIOLATION
+        // Perhaps this asks the RoboRIO to report its voltage, so it should only be called in real mode
+        assertEquals(12.0, RobotController.getBatteryVoltage(), 0.01);
+
+        // Simplify the test: just ask for the battery voltage -- still fails
+        // RobotController.getBatteryVoltage();
+    }
+
+    // See if some other part of RobotController is functional in simulation
+    @Test
+    public void robotControllerMethodsTest() {
+        // getting FPGA time does not fail call does not throw an exception.
+        RobotController.getFPGATime();
+
+        // Can ask for the version. 2018
+        // does return HALUtil.getFPGAVersion();
+        // System.out.println(RobotController.getFPGAVersion());
+        assertEquals(2018, RobotController.getFPGAVersion());
+
+        // Can ask for the Revision. 0
+        // Number should be 12 bits major, 8 bits minor, 12 bits build
+        // does return HALUtil.getFPGARevision();
+        // System.out.println(RobotController.getFPGARevision());
+        assertEquals(0, RobotController.getFPGARevision());
+
+        // May not ask for the Serial Number.
+        // Crashes in native code. Probably trying to connect to the RoboRIO: HALUtil.getSerialNumber();
+        // System.out.println(RobotController.getSerialNumber());
+        // assertEquals(0, RobotController.getSerialNumber());
+
+        // Get the team number
+        // Access violation
+        // System.out.println(RobotController.getTeamNumber());
+        // assertEquals(972, RobotController.getTeamNumber());
+    }
+   
+}
