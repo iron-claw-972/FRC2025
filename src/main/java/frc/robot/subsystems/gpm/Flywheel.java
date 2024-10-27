cd81613f2b4f17114440e3cbca13abd49b4911c3
Kyle-Eldridge <eldk6798@lgsstudent.org>
2024-10-26 19:44:59 -0700

    Add some simulation support, add left and right controllers

--- src/main/java/frc/robot/subsystems/gpm/Flywheel.java
+++ src/main/java/frc/robot/subsystems/gpm/Flywheel.java
@@ -18,12 +18,10 @@ import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.LinearSystemLoop;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.Constants;
 import frc.robot.constants.ShooterConstants;
 
 /**
@@ -31,58 +29,83 @@ import frc.robot.constants.ShooterConstants;
  * flywheel.
  */
 public class Flywheel extends SubsystemBase {
-  private static final int kMotorPort = 0;
-  private static final int kEncoderAChannel = 0;
-  private static final int kEncoderBChannel = 1;
 
-  double referenceState;
+  private double leftSetpoint;
+  private double rightSetpoint;
 
-  private static final double kFlywheelMomentOfInertia = 0.00032; // kg * m^2
+  // 1 motor per side
+  private static final DCMotor gearbox = DCMotor.getNeoVortex(1);
+
+  // TODO: Check if this is for 1 side or both sides
+  private static final double momentOfInertia = 0.00032; // kg * m^2
 
   // Reduction between motors and encoder, as output over input. If the flywheel spins slower than
   // the motors, this number should be greater than one.
-  private static final double kFlywheelGearing = 1.0;
+  private static final double gearing = 1.0;
 
   // The plant holds a state-space model of our flywheel. This system has the following properties:
   //
-  // States: [velocity], in radians per second.
+  // States: [velocity], in rotations per minute.
   // Inputs (what we can "put in"): [voltage], in volts.
-  // Outputs (what we can measure): [velocity], in radians per second.
-  private final LinearSystem<N1, N1, N1> m_flywheelPlant =
-      LinearSystemId.createFlywheelSystem(
-          DCMotor.getNEO(2), kFlywheelMomentOfInertia, kFlywheelGearing);
+  // Outputs (what we can measure): [velocity], in rotations per minute.
+  // TODO: Can multiple controllers use teh same plant?
+  private final LinearSystem<N1, N1, N1> leftFlywheelPlant =
+    LinearSystemId.createFlywheelSystem(
+      DCMotor.getNeoVortex(1), momentOfInertia, gearing);
+  private final LinearSystem<N1, N1, N1> rightFlywheelPlant =
+    LinearSystemId.createFlywheelSystem(
+      DCMotor.getNeoVortex(1), momentOfInertia, gearing);
 
   // The observer fuses our encoder data and voltage inputs to reject noise.
-  private final KalmanFilter<N1, N1, N1> m_observer =
-      new KalmanFilter<>(
-          Nat.N1(),
-          Nat.N1(),
-          m_flywheelPlant,
-          VecBuilder.fill(3.0), // How accurate we think our model is
-          VecBuilder.fill(0.01), // How accurate we think our encoder
-          // data is
-          0.020);
-
+  // TODO: Can both sides us the same filter?
+  private final KalmanFilter<N1, N1, N1> leftObserver =
+    new KalmanFilter<>(
+      Nat.N1(),
+      Nat.N1(),
+      leftFlywheelPlant,
+      VecBuilder.fill(3.0), // How accurate we think our model is
+      VecBuilder.fill(0.01), // How accurate we think our encoder
+      // data is
+      Constants.LOOP_TIME);
+  private final KalmanFilter<N1, N1, N1> rightObserver =
+    new KalmanFilter<>(
+      Nat.N1(),
+      Nat.N1(),
+      rightFlywheelPlant,
+      VecBuilder.fill(3.0), // How accurate we think our model is
+      VecBuilder.fill(0.01), // How accurate we think our encoder
+      // data is
+      Constants.LOOP_TIME);
+    
   // A LQR uses feedback to create voltage commands.
-  private final LinearQuadraticRegulator<N1, N1, N1> m_controller =
-      new LinearQuadraticRegulator<>(
-          m_flywheelPlant,
-          VecBuilder.fill(8.0), // qelms. Velocity error tolerance, in radians per second. Decrease
-          // this to more heavily penalize state excursion, or make the controller behave more
-          // aggressively.
-          VecBuilder.fill(12.0), // relms. Control effort (voltage) tolerance. Decrease this to more
-          // heavily penalize control effort, or make the controller less aggressive. 12 is a good
-          // starting point because that is the (approximate) maximum voltage of a battery.
-          0.020); // Nominal time between loops. 0.020 for TimedRobot, but can be
+  private final LinearQuadraticRegulator<N1, N1, N1> leftController =
+    new LinearQuadraticRegulator<>(
+      leftFlywheelPlant,
+      VecBuilder.fill(8.0), // qelms. Velocity error tolerance, in radians per second. Decrease
+      // this to more heavily penalize state excursion, or make the controller behave more
+      // aggressively.
+      VecBuilder.fill(12.0), // relms. Control effort (voltage) tolerance. Decrease this to more
+      // heavily penalize control effort, or make the controller less aggressive. 12 is a good
+      // starting point because that is the (approximate) maximum voltage of a battery.
+      Constants.LOOP_TIME); // Nominal time between loops. Constants.LOOP_TIME for TimedRobot, but can be
+  // lower if using notifiers.
+  private final LinearQuadraticRegulator<N1, N1, N1> rightController =
+    new LinearQuadraticRegulator<>(
+      rightFlywheelPlant,
+      VecBuilder.fill(8.0), // qelms. Velocity error tolerance, in radians per second. Decrease
+      // this to more heavily penalize state excursion, or make the controller behave more
+      // aggressively.
+      VecBuilder.fill(12.0), // relms. Control effort (voltage) tolerance. Decrease this to more
+      // heavily penalize control effort, or make the controller less aggressive. 12 is a good
+      // starting point because that is the (approximate) maximum voltage of a battery.
+      Constants.LOOP_TIME); // Nominal time between loops. Constants.LOOP_TIME for TimedRobot, but can be
   // lower if using notifiers.
 
   // The state-space loop combines a controller, observer, feedforward and plant for easy control.
-  private final LinearSystemLoop<N1, N1, N1> m_loop =
-      new LinearSystemLoop<>(m_flywheelPlant, m_controller, m_observer, 12.0, 0.020);
-
-  // An encoder set up to measure flywheel velocity in radians per second.
-  private final Encoder m_encoder = new Encoder(kEncoderAChannel, kEncoderBChannel);
-
+  private final LinearSystemLoop<N1, N1, N1> leftLoop =
+    new LinearSystemLoop<>(leftFlywheelPlant, leftController, leftObserver, 12.0, Constants.LOOP_TIME);
+  private final LinearSystemLoop<N1, N1, N1> rightLoop =
+    new LinearSystemLoop<>(rightFlywheelPlant, rightController, rightObserver, 12.0, Constants.LOOP_TIME);
   
   private final CANSparkFlex leftMotor = new CANSparkFlex(ShooterConstants.LEFT_MOTOR_ID, MotorType.kBrushless);
   private final CANSparkFlex rightMotor = new CANSparkFlex(ShooterConstants.RIGHT_MOTOR_ID, MotorType.kBrushless);
@@ -90,14 +113,21 @@ public class Flywheel extends SubsystemBase {
   private final RelativeEncoder leftMotorEncoder = leftMotor.getEncoder();
   private final RelativeEncoder rightMotorEncoder = rightMotor.getEncoder();
 
+  private FlywheelSim leftSim;
+  private FlywheelSim rightSim;
+
   public Flywheel() {
-    // We go 2 pi radians per 4096 clicks.
-    //m_encoder.setDistancePerPulse(2.0 * Math.PI / 4096.0);
-    m_loop.reset(VecBuilder.fill(m_encoder.getRate()));
-    referenceState = 0;
+    leftLoop.reset(VecBuilder.fill(leftMotorEncoder.getVelocity()));
+    rightLoop.reset(VecBuilder.fill(leftMotorEncoder.getVelocity()));
+    leftSetpoint = 0;
     rightMotor.follow(leftMotor);
     rightMotor.setInverted(false);
-	leftMotor.setInverted(true);
+	  leftMotor.setInverted(true);
+
+    if(RobotBase.isSimulation()){
+      leftSim = new FlywheelSim(leftFlywheelPlant, gearbox, gearing);
+      rightSim = new FlywheelSim(rightFlywheelPlant, gearbox, gearing);
+    }
   }
 
 
@@ -105,21 +135,54 @@ public class Flywheel extends SubsystemBase {
   public void periodic() {
     // Sets the target speed of our flywheel. This is similar to setting the setpoint of a
     // PID controller.
+    leftLoop.setNextR(VecBuilder.fill(leftSetpoint));
+    rightLoop.setNextR(VecBuilder.fill(rightSetpoint));
     
-    m_loop.setNextR(VecBuilder.fill(referenceState));
-    
-
     // Correct our Kalman filter's state vector estimate with encoder data.
-    m_loop.correct(VecBuilder.fill(m_encoder.getRate()));
+    leftLoop.correct(VecBuilder.fill(leftMotorEncoder.getVelocity()));
+    rightLoop.correct(VecBuilder.fill(rightMotorEncoder.getVelocity()));
 
     // Update our LQR to generate new voltage commands and use the voltages to predict the next
     // state with out Kalman filter.
-    m_loop.predict(0.020);
+    leftLoop.predict(Constants.LOOP_TIME);
+    rightLoop.predict(Constants.LOOP_TIME);
 
     // Send the new calculated voltage to the motors.
     // voltage = duty cycle * battery voltage, so
     // duty cycle = voltage / battery voltage
-    double nextVoltage = m_loop.getU(0);
+    double nextVoltage = leftLoop.getU(0);
     leftMotor.setVoltage(nextVoltage);
+    nextVoltage = rightLoop.getU(0);
+    rightMotor.setVoltage(nextVoltage);
+  }
+
+  @Override
+  public void simulationPeriodic(){
+    leftSim.setInputVoltage(leftMotor.getBusVoltage());
+    rightSim.setInputVoltage(leftMotor.getBusVoltage());
+    leftSim.update(Constants.LOOP_TIME);
+    rightSim.update(Constants.LOOP_TIME);
+  }
+
+  public void setSpeed(double leftSpeed, double rightSpeed){
+    leftSetpoint = leftSpeed;
+    rightSetpoint = rightSpeed;
+  }
+  public void setSpeed(double speed){
+    setSpeed(speed, speed);
+  }
+  public double getLeftSpeed(){
+    // I couldn't find a class that supports simulated encoder velocities for a RelativeEncoder. TODO: Find one if it exists
+    if(RobotBase.isSimulation()){
+      return leftSim.getAngularVelocityRPM();
+    }
+    return leftMotorEncoder.getVelocity();
+  }
+  public double getRightSpeed(){
+    // I couldn't find a class that supports simulated encoder velocities for a RelativeEncoder. TODO: Find one if it exists
+    if(RobotBase.isSimulation()){
+      return rightSim.getAngularVelocityRPM();
+    }
+    return rightMotorEncoder.getVelocity();
   }
 }
