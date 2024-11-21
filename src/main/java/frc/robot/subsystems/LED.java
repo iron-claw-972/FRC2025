6b353e1e96b7b16d85a780114d1a3a8e37f3462b
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2024-11-20 16:37:21 -0800

    Add setSection

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -38,4 +38,18 @@ public class LED extends SubsystemBase {
     //         this.colorValue = colorValue;
     //     }
     // }
+
+    public void setSection(int r, int g, int b, int start, int end){
+        candle.setLEDs(r, g, b, 0, start, end);
+    }
+    public void alternate(int r1, int g1, int b1, int r2, int g2, int b2, int size, int offset, int total){
+        for(int i = -offset; i < total; i+=size){
+            boolean color2 = ((i-offset)/size)%2==0;
+            if(color2){
+                setSection(r2, g2, b2, i, i+size-1);
+            }else{
+                setSection(r1, g1, b1, i, i+size-1);
+            }
+        }
+    }
 }
