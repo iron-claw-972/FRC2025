8014f671b7df347012da334d69b3a264c746570f
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2025-01-22 16:47:39 -0800

    Fix bug in driver assist

--- src/main/java/frc/robot/commands/vision/TestVisionDistance.java
+++ src/main/java/frc/robot/commands/vision/TestVisionDistance.java
@@ -5,7 +5,6 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.constants.Constants;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.util.LogManager;
 import frc.robot.util.Vision;
