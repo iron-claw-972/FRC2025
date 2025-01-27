a734da930b68e7b75ac778ec34ff4cff2b4bdbe3
iefomit <108955303+iefomit@users.noreply.github.com>
2025-01-27 15:39:38 -0800

    Formatting, New Encoder & Sim

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,35 +2,38 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.sim.TalonFXSSimState;
+
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.util.Units;
-
-
-
-
 
 public class Intake extends SubsystemBase {
 
     // TODO put in proper id
     private final TalonFX rollMotor = new TalonFX(70);
     private final TalonFX stowMotor = new TalonFX(68);
+    private TalonFXSSimState encoderSim;
 
     private final PIDController stowPID = new PIDController(0, 0, 0);
-
     private final double motorVoltage = 12.0;
+
+    Encoder encoder = new Encoder();
+    EncoderSim simEncoder = new EncoderSim(encoder);
 
     public Intake() {
 
-        //TODO set proper tolerance
+        // TODO set proper tolerance
         stowPID.setTolerance(.5);
 
         if (RobotBase.isSimulation()) {
             // TODO: Add simulation-specific behavior if needed
+            encoderSim = stowMotor.getSimState();
         }
-
 
         publish();
     }
@@ -44,10 +47,9 @@ public class Intake extends SubsystemBase {
         publish();
         stowMotor.set(stowPID.calculate(getStowPosition()));
 
-
     }
 
-    public double getStowPosition(){
+    public double getStowPosition() {
         return Units.rotationsToDegrees(stowMotor.getPosition().getValueAsDouble());
     }
 
@@ -56,23 +58,23 @@ public class Intake extends SubsystemBase {
         return false;
     }
 
-    public void setAngle(double angle){
+    public void setAngle(double angle) {
         stowPID.setSetpoint(angle);
     }
 
-    public void setSpeed(double power){
+    public void setSpeed(double power) {
         rollMotor.set(power);
     }
 
-    public void stow(){
+    public void stow() {
         stowPID.setSetpoint(90);
     }
 
-    public void deactivate(){
+    public void deactivate() {
         rollMotor.set(0);
     }
 
-    public void activate(){
+    public void activate() {
         stowPID.setSetpoint(0);
         rollMotor.set(.8);
     }
