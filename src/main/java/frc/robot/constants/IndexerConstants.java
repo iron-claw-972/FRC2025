6b02740af452506f11b2998148f27ab81342a7bf
Arnav495 <arnieincyberland@gmail.com>
2025-01-30 10:32:07 -0800

    Remove unnecesary indexer constant.

--- src/main/java/frc/robot/constants/IndexerConstants.java
+++ src/main/java/frc/robot/constants/IndexerConstants.java
@@ -3,7 +3,6 @@ package frc.robot.constants;
 public class IndexerConstants {
 	public static final double speed = 0.5;
 
-	public static final double timeout = 2.0; // in seconds
 	public static final double runForExtra = 1.0; // time to run after sensor finishes
 
 	public static final double momentOfInertia = 1.0; // TODO: actual value
