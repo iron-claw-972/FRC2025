b58329c4762587d77aa4d6871056944ef839ab15
Arnav495 <arnieincyberland@gmail.com>
2025-01-30 10:30:54 -0800

    Removed code to control state from Indexer

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -5,9 +5,7 @@ import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.simulation.DIOSim;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -19,26 +17,15 @@ import frc.robot.util.LogManager;
 import frc.robot.util.LogManager.LogLevel;
 
 public class Indexer extends SubsystemBase {
-	public enum Status {
-		RUNNING, FINISHING, IDLE, ERROR
-	};
-
-	private Status status;
-	private Timer timeSinceRequest;
-	private boolean sensorToggled;
-
 	private SparkMax motor;
 	private DigitalInput sensor;
+
 	private FlywheelSim flywheelSim;
 	private DIOSim sensorSim;
 
 	public Indexer() {
 		motor = new SparkMax(IdConstants.INDEXER_MOTOR, MotorType.kBrushless);
 		sensor = new DigitalInput(IdConstants.INDEXER_SENSOR);
-
-		timeSinceRequest = new Timer();
-		sensorToggled = false;
-		status = Status.IDLE;
 
 		if (Robot.isSimulation()) {
 			flywheelSim = new FlywheelSim(LinearSystemId.createFlywheelSystem(DCMotor.getNEO(1),
@@ -51,9 +38,22 @@ public class Indexer extends SubsystemBase {
 	}
 
 	/**
+	 * Set the indexer motor's speed
+	 * setMotor(0) stops the indexer
+	 * 
+	 * @param speed the speed to set the motor to
+	 */
+	public void setMotor(double speed) {
+		if (speed <= 0.001)
+			motor.stopMotor();
+		else
+			motor.set(speed);
+	}
+
+	/**
 	 * @return the motor position in rotations
 	 */
-	private double getMotor() {
+	public double getMotor() {
 		if (Robot.isReal()) {
 			return motor.getEncoder().getVelocity();
 		} else {
@@ -64,81 +64,35 @@ public class Indexer extends SubsystemBase {
 	/**
 	 * Gets the sensor's state
 	 * true means nothing is there, false means something is there
+	 * 
+	 * @return the sensor's state
 	 */
-	private boolean getSensorValue() {
+	public boolean getSensorValue() {
 		if (Robot.isReal()) {
 			return sensor.get();
 		} else {
 			return sensorSim.getValue();
 		}
 	}
+	
+	/**
+	 * Set the sensor's state
+	 * true means nothing is there, false means something is there
+	 * Use for simulation (do NOT call on a real robot)
+	 * 
+	 * @param value the state to set the sensor to
+	 */
+	public void setSensorState(boolean value) {
+		assert(Robot.isSimulation());
+		sensorSim.setValue(value);
+	};
 
 	@Override
-	public void periodic() {
-		switch (status) {
-			case IDLE:
-			case ERROR:
-				motor.stopMotor();
-				break;
-
-			case RUNNING:
-				motor.set(IndexerConstants.speed);
-
-				// trigger after sensor goes on, broken, on; or __/--\__
-				if (getSensorValue() == false) {
-					sensorToggled = true;
-				} else if (getSensorValue() == true && sensorToggled == true) {
-					timeSinceRequest.reset(); // reuse the same timer
-					status = Status.FINISHING;
-				}
-
-				if (timeSinceRequest.hasElapsed(IndexerConstants.timeout)) {
-					timeSinceRequest.stop();
-					LogManager.logFault("Indexer error", AlertType.kWarning);
-					status = Status.ERROR; // if we've taken too long
-				}
-
-				break;
-
-			case FINISHING:
-				motor.set(IndexerConstants.speed);
-
-				if (timeSinceRequest.hasElapsed(IndexerConstants.runForExtra)) {
-					timeSinceRequest.stop();
-					motor.stopMotor();
-				}
-
-				break;
-		}
-	}
+	public void periodic() { }
 
 	@Override
 	public void simulationPeriodic() {
 		flywheelSim.setInput(motor.getOutputCurrent() * Constants.ROBOT_VOLTAGE);
 		flywheelSim.update(Constants.LOOP_TIME);
-	}
-
-	/*
-	 * Run the indexer
-	 */
-	public void run() {
-		status = Status.RUNNING;
-		sensorToggled = false;
-		timeSinceRequest.restart();
-	}
-
-	/*
-	 * Stop the indexer.
-	 */
-	public void stop() {
-		status = Status.IDLE;
-		timeSinceRequest.stop();
-	}
-
-	/*
-	 * Get the indexer status.
-	 */
-	public Status getStatus() {
-		return status;
 	}
 }
