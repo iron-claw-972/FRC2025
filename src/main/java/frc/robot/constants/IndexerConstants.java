e042fc36c5d661f1fefd001c787bb197b4599333
Arnav495 <arnieincyberland@gmail.com>
2025-01-29 16:19:22 -0800

    Initial indexer code.

---
+++ src/main/java/frc/robot/constants/IndexerConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.constants;
+
+public class IndexerConstants {
+	public static final double speed = 0.5;
+
+	public static final double timeout = 2.0; // in seconds
+	public static final double runForExtra = 1.0; // time to run after sensor finishes
+}
+
--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -1,7 +1,93 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.IdConstants;
+import frc.robot.constants.IndexerConstants;
 
 public class Indexer extends SubsystemBase {
-    // TODO: finish
+	enum Status {
+		RUNNING, FINISHING, IDLE, ERROR
+	};
+
+	private Status status;
+	private Timer timeSinceRequest;
+	private boolean sensorToggled;
+
+	private SparkMax motor;
+	private DigitalInput sensor;
+
+	public Indexer() {
+		motor = new SparkMax(IdConstants.INDEXER_MOTOR, MotorType.kBrushless); // FIXME: is it brushless?
+		sensor = new DigitalInput(IdConstants.INDEXER_SENSOR);
+		timeSinceRequest = new Timer();
+		sensorToggled = false;
+		status = Status.IDLE;
+	}
+
+	@Override
+	public void periodic() {
+		switch (status) {
+			case IDLE:
+			case ERROR:
+				motor.stopMotor();
+				break;
+
+			case RUNNING:
+				motor.set(IndexerConstants.speed);
+
+				// trigger after sensor goes on, broken, on; or __/--\__
+				if (sensor.get() == false) {
+					sensorToggled = true;
+				} else if (sensor.get() == true && sensorToggled == true) {
+					timeSinceRequest.reset(); // reuse the same timer
+					status = Status.FINISHING;
+				}
+
+				if (timeSinceRequest.hasElapsed(IndexerConstants.timeout)) {
+					timeSinceRequest.stop();
+					status = Status.ERROR; // if we've taken too long
+				}
+
+				break;
+
+			case FINISHING:
+				motor.set(IndexerConstants.speed);
+
+				if (timeSinceRequest.hasElapsed(IndexerConstants.runForExtra)) {
+					timeSinceRequest.stop();
+					motor.stopMotor();
+				}
+
+				break;
+		}
+	}
+
+	/*
+	 * Run the indexer
+	 */
+	public void run() {
+		status = Status.RUNNING;
+		sensorToggled = false;
+		timeSinceRequest.restart();
+	}
+
+	/*
+	 * Stop the indexer.
+	 */
+	public void stop() {
+		this.status = Status.IDLE;
+		this.timeSinceRequest.stop();
+	}
+
+	/*
+	 * Get the indexer status.
+	 */
+	public Status getStatus() {
+		return this.status;
+	}
 }
