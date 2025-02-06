0b89ec4f81400a71b0a052084c86e804d89f0bb7
eileha <eileenhan369@gmail.com>
2025-02-05 17:26:12 -0800

    added the timer back in again

--- src/main/java/frc/robot/commands/gpm/OuttakeCoralBasic.java
+++ src/main/java/frc/robot/commands/gpm/OuttakeCoralBasic.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.gpm;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Outtake;
 
@@ -12,7 +13,7 @@ public class OuttakeCoralBasic extends Command {
     private Outtake outtake;
 
     // states the outtake may take
-    enum State { EMPTY, LOADED, MOVING, JAMMED, REVERSING, DONE }
+    enum State { EMPTY, LOADED, MOVING, REVERSING, DONE }
 
     private State state;
 
@@ -30,6 +31,8 @@ public class OuttakeCoralBasic extends Command {
         if (outtake.coralLoaded()) {
             // coral is present, ejecting makes sense
             state = State.LOADED;
+            // wheels start spinning
+            outtake.outtake();
         }
         else {
             // no coral present, ejecting does not make sense
@@ -40,10 +43,12 @@ public class OuttakeCoralBasic extends Command {
     public void execute(){
         // bump the timer
         ticks++;
-        /*if(ticks>200){
+
+        // if the outtake goes for too long, just give up. Something is seriously wrong.
+        // 100 ticks is 2 seconds; autonomous is only 15 seconds
+        if(ticks > 100){
             state = State.DONE;
         }
-            */
 
         switch (state) {
             case EMPTY:
@@ -54,35 +59,39 @@ public class OuttakeCoralBasic extends Command {
             // waiting for ejected to become true
             // FIXME: is this supposed to be coralLoaded????
             if (outtake.coralEjecting()) {
+                // at this point, ticks represents how long it took to move the coral to the ejecting sensor.
+                SmartDashboard.putNumber("Coral Ejection Time", ticks * 0.020);
+
+                // reset the timer
                 ticks = 0;
+                // we know the coral is moving
                 state = State.MOVING;
-                outtake.outtake();
             }
             break;
 
             case MOVING:
             // waiting for ejected to become false (success)
-            if(!outtake.coralEjecting()){
+            if (!outtake.coralEjecting()){
+                // coral has gone all the way through.
+                SmartDashboard.putNumber("Coral Transit Time", ticks * 0.020);
                 state = State.DONE;
             }
             // waiting for a timeout; 100 ticks is 2 seconds.
             if (ticks > 25) {
-                state = State.JAMMED;
+                // reverse the motor, at -0.1: sometimes did not have the power to reverse, at -0.15: ejected all the way back, hit the funnel
+                outtake.setMotor(-0.125);
+
+                SmartDashboard.putNumber("Coral Transit Time", ticks * 0.020);
+
+                state = State.REVERSING;
             }
-            break;
-
-            case JAMMED:
-            // reverse the motor, at -0.1: sometimes did not have the power to reverse, at -0.15: ejected all the way back, hit the funnel
-            outtake.setMotor(-0.125);
-
-            state = State.REVERSING;
             break;
 
             case REVERSING:
             // waiting for ejected to be false
-            if(!outtake.coralEjected()){
-                state = State.LOADED;
-                // outtake.stop();
+            if (!outtake.coralEjecting()){
+                // we are done. When command finishes, the motor will be stopped.
+                state = State.DONE;
             }
             break;
 
--- src/main/java/frc/robot/subsystems/Outtake.java
+++ src/main/java/frc/robot/subsystems/Outtake.java
@@ -7,9 +7,13 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkFlexConfig;
 
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
 import frc.robot.constants.IdConstants;
 
 public class Outtake extends SubsystemBase {
