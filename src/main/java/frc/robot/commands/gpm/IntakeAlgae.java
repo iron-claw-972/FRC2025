415896c768d5d352f35f3c35f4bc831f5f576593
iefomit <108955303+iefomit@users.noreply.github.com>
2025-02-12 16:29:01 -0800

    Doing Intake Algae Command

--- src/main/java/frc/robot/commands/gpm/IntakeAlgae.java
+++ src/main/java/frc/robot/commands/gpm/IntakeAlgae.java
@@ -1,11 +1,41 @@
 package frc.robot.commands.gpm;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
 
 public class IntakeAlgae extends Command {
-    // TODO: finish
-    public IntakeAlgae(Intake intake){
+    private final Intake intake;
+    private final Timer timer = new Timer();
+    private static final double INTAKE_TIME = 5.0;
 
+    public IntakeAlgae(Intake intake) {
+        this.intake = intake;
+        addRequirements(intake);
+    }
+
+    @Override
+    public void initialize() {
+        intake.setAngle(90);
+        intake.setSpeed(1.0);
+        timer.reset();
+        timer.start();
+    }
+
+    @Override
+    public void execute() {
+        if (timer.hasElapsed(INTAKE_TIME)) {
+            intake.deactivate();
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return timer.hasElapsed(INTAKE_TIME);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        intake.deactivate();
     }
 }
