c73f0381f62ca3c50b255b5bbdabb59c2688719a
Kyle-Eldridge <eldk6798@lgsstudent.org>
2025-02-13 15:44:30 -0800

    Remove intake from OuttakeCoral

--- src/main/java/frc/robot/commands/gpm/OuttakeCoral.java
+++ src/main/java/frc/robot/commands/gpm/OuttakeCoral.java
@@ -1,29 +1,27 @@
 package frc.robot.commands.gpm;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Outtake;
 
 public class OuttakeCoral extends Command {
     // TODO: finish
-    Intake intake;
-    public OuttakeCoral(Intake intake){
-        this.intake = intake;
-        addRequirements(intake);
+    public OuttakeCoral(Outtake outtake){
+        addRequirements(outtake);
     }
 
     @Override
     public void initialize(){
-        intake.setSpeed(-.5);
+        
     }
 
     @Override
     public boolean isFinished(){
-        return intake.hasCoral();
+        return true;
     }
 
     @Override
     public void end(boolean interrupted){
-        intake.setSpeed(0);
+        
     }
 
 
