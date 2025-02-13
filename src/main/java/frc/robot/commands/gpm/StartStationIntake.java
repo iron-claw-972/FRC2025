08a4e957302b9247f9da6d937875b2986b965021
moo <moogoesmeow123@gmail.com>
2025-02-13 15:18:10 -0800

    Do start station intake

--- src/main/java/frc/robot/commands/gpm/StartStationIntake.java
+++ src/main/java/frc/robot/commands/gpm/StartStationIntake.java
@@ -1,11 +1,33 @@
 package frc.robot.commands.gpm;
 
+import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
 
 public class StartStationIntake extends Command {
     // TODO: finish and possibly rename
+
+    Intake intake;
+
     public StartStationIntake(Intake intake){
-        
+        this.intake = intake;
+        addRequirements(intake);
+    }
+
+    @Override
+    public void initialize(){
+        intake.setAngle(10);
+        intake.activate();
+    }
+
+    @Override
+    public boolean isFinished(){
+        return intake.hasCoral();
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        intake.deactivate();
+        intake.stow();
     }
 }
