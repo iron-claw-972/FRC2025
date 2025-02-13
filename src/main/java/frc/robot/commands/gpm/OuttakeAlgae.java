e0cd059e19fc77eee7c4bfaeff916edcb317f2fa
moo <moogoesmeow123@gmail.com>
2025-02-13 15:31:17 -0800

    add outtake coral.java

--- src/main/java/frc/robot/commands/gpm/OuttakeAlgae.java
+++ src/main/java/frc/robot/commands/gpm/OuttakeAlgae.java
@@ -1,7 +1,6 @@
 package frc.robot.commands.gpm;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Intake;
 
 public class OuttakeAlgae extends Command {
--- src/main/java/frc/robot/commands/gpm/OuttakeCoral.java
+++ src/main/java/frc/robot/commands/gpm/OuttakeCoral.java
@@ -1,12 +1,30 @@
 package frc.robot.commands.gpm;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Elevator;
-import frc.robot.subsystems.Outtake;
+import frc.robot.subsystems.Intake;
 
 public class OuttakeCoral extends Command {
     // TODO: finish
-    public OuttakeCoral(Outtake outtake, Elevator elevator){
-
+    Intake intake;
+    public OuttakeCoral(Intake intake){
+        this.intake = intake;
+        addRequirements(intake);
     }
+
+    @Override
+    public void initialize(){
+        intake.setSpeed(-.5);
+    }
+
+    @Override
+    public boolean isFinished(){
+        return intake.hasCoral();
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        intake.setSpeed(0);
+    }
+
+
 }
