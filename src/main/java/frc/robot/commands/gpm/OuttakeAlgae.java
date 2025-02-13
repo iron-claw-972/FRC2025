0f8f69f9bf9cb3a92be2915775bc53cdbbe41473
moo <moogoesmeow123@gmail.com>
2025-02-13 15:29:55 -0800

    make outtake algae.java
    is this the right branch for this?

--- src/main/java/frc/robot/commands/gpm/OuttakeAlgae.java
+++ src/main/java/frc/robot/commands/gpm/OuttakeAlgae.java
@@ -1,11 +1,31 @@
 package frc.robot.commands.gpm;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Intake;
 
 public class OuttakeAlgae extends Command {
     // TODO: finish
+    Intake intake;
     public OuttakeAlgae(Intake intake){
-
+        this.intake = intake;
+        addRequirements(intake);
     }
+
+    @Override
+    public void initialize(){
+        intake.setSpeed(-.5);
+    }
+
+    @Override
+    public boolean isFinished(){
+        return true; //TODO how do we know when algae is out or in?
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        intake.setSpeed(0);
+    }
+
+
 }
