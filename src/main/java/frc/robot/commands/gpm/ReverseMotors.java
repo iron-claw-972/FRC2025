7a06d8398936e74df971aa2743982f4314052424
moo <moogoesmeow123@gmail.com>
2025-02-13 15:33:40 -0800

    add reverse motors
    outtake don't work yet

--- src/main/java/frc/robot/commands/gpm/ReverseMotors.java
+++ src/main/java/frc/robot/commands/gpm/ReverseMotors.java
@@ -6,7 +6,28 @@ import frc.robot.subsystems.Outtake;
 
 public class ReverseMotors extends Command {
     // TODO: finish
+    Intake intake;
+    Outtake outtake;
     public ReverseMotors(Intake intake, Outtake outtake){
-        
+        this.intake = intake;
+        this.outtake = outtake;
+        addRequirements(intake,outtake);
+    }
+
+    @Override
+    public void initialize(){
+        intake.setSpeed(-.5);
+        //outtake.setSpeed(-.5);
+    }
+
+    @Override
+    public boolean isFinished(){
+        return true; //TODO when is this finished?
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        intake.setSpeed(0);
+        //outtake.setSpeed(0);
     }
 }
