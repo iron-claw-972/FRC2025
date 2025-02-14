70608784bb893f2bdd23f90bd341fe677c67b994
moo <moogoesmeow123@gmail.com>
2025-02-14 09:17:18 -0800

    refactor finishstationintake.java to use intakecoralhelper

--- src/main/java/frc/robot/commands/gpm/FinishStationIntake.java
+++ src/main/java/frc/robot/commands/gpm/FinishStationIntake.java
@@ -7,7 +7,6 @@ import frc.robot.subsystems.Indexer;
 import frc.robot.subsystems.Intake;
 
 public class FinishStationIntake extends Command {
-    // TODO: finish and possibly rename
     // TODO: Intake needs to move down first, then it's basically the same as a normal intake
     // Maybe use the IntakeCoralHelper command
 
@@ -20,24 +19,24 @@ public class FinishStationIntake extends Command {
         this.indexer = indexer;
         this.elevator = elevator;
         addRequirements(intake, indexer, elevator);
+        new IntakeCoralHelper(intake, indexer);
     }
 
     @Override
     public void initialize(){
         intake.unstow();
-        intake.activate();
-        indexer.run();
         elevator.setSetpoint(ElevatorConstants.INTAKE_SETPOINT);
     }
 
     @Override
     public boolean isFinished(){
-        return true; //TODO Finish when indexer finishes detecting
+        return indexer.getSensorValue(); //TODO Finish when indexer finishes detecting
     }
 
     @Override
     public void end(boolean interrupted){
         intake.stow();
+        intake.deactivate();
         indexer.stop();
     }
 }
