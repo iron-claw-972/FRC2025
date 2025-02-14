8fbe70ca0f348e01a6095aff7fa474f31680909a
moo <moogoesmeow123@gmail.com>
2025-02-13 16:03:17 -0800

    modify to not use timer, because already stops on button release

--- src/main/java/frc/robot/commands/gpm/IntakeAlgae.java
+++ src/main/java/frc/robot/commands/gpm/IntakeAlgae.java
@@ -1,15 +1,10 @@
 package frc.robot.commands.gpm;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
 
 public class IntakeAlgae extends Command {
     private final Intake intake;
-    private final Timer timer = new Timer();
-
-    // FIXME: Modify to not use a timer. We should intake until the driver releases the button and interrrupts the command
-    private static final double INTAKE_TIME = 5.0;
 
     public IntakeAlgae(Intake intake) {
         this.intake = intake;
@@ -20,20 +15,6 @@ public class IntakeAlgae extends Command {
     public void initialize() {
         intake.setAngle(90);
         intake.setSpeed(1.0);
-        timer.reset();
-        timer.start();
-    }
-
-    @Override
-    public void execute() {
-        if (timer.hasElapsed(INTAKE_TIME)) {
-            intake.deactivate();
-        }
-    }
-
-    @Override
-    public boolean isFinished() {
-        return timer.hasElapsed(INTAKE_TIME);
     }
 
     @Override
