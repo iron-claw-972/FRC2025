24f618533a218c8f6ec8cae225563554e775edaf
Kyle-Eldridge <eldk6798@lgsstudent.org>
2025-02-16 18:40:00 -0800

    Move DefaultDriveCommand to a thread

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -53,7 +53,7 @@ public class RobotContainer {
   private ShuffleBoardManager shuffleboardManager = null;
 
   private Thread odometryThread = null;
-  private Thread moduleLQRThread = null;
+  private Thread drivetrainThread = null;
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -127,17 +127,17 @@ public class RobotContainer {
           drive.updateOdometry();
         }
       });
-      moduleLQRThread = new Thread(()->{
+      drivetrainThread = new Thread(()->{
         long nextUpdate = System.currentTimeMillis();
-        while(!moduleLQRThread.isInterrupted()){
+        while(!drivetrainThread.isInterrupted()){
           if(System.currentTimeMillis() >= nextUpdate){
-            drive.runModuleLQR();
+            drive.drive(driver);
             nextUpdate += 20;
           }
         }
       });
       odometryThread.start();
-      moduleLQRThread.start();
+      drivetrainThread.start();
     }
   }
 
@@ -203,7 +203,7 @@ public class RobotContainer {
 
   public void interruptThreads(){
     odometryThread.interrupt();
-    moduleLQRThread.interrupt();
+    drivetrainThread.interrupt();
   }
 }
 
