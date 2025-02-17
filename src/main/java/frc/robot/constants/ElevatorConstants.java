469558650e569b7ded6398ed6d4583110b495644
chem7397 <113394068+chem7397@users.noreply.github.com>
2025-02-17 15:21:22 -0800

    elevator setpoints

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -27,9 +27,9 @@ public class ElevatorConstants {
     public static final double STOW_SETPOINT = 0;
     public static final double INTAKE_SETPOINT = 0;
     public static final double L1_SETPOINT = 0;
-    public static final double L2_SETPOINT = 0.08;
-    public static final double L3_SETPOINT = 0.5;
-    public static final double L4_SETPOINT = 1.18;
+    public static final double L2_SETPOINT = 0.72;
+    public static final double L3_SETPOINT = 1.1;
+    public static final double L4_SETPOINT = 1.66;
 
     public static final double CENTER_OF_MASS_HEIGHT_STOWED = Units.inchesToMeters(8.933);
     public static final double CENTER_OF_MASS_HEIGHT_EXTENDED = Units.inchesToMeters(16.931);
