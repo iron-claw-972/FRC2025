780b6a1de7cf1bf5be33e1be5a272c3c864a3cf3
Kyle-Eldridge <eldk6798@lgsstudent.org>
2025-02-18 13:26:44 -0800

    Fix merge error

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -8,6 +8,7 @@ import au.grapplerobotics.interfaces.LaserCanInterface;
 import au.grapplerobotics.simulation.MockLaserCan;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
