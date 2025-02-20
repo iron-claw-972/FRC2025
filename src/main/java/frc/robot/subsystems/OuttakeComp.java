2801d0194aeb1712c286beff854a657b3be076d7
GLRoylance <GLRoylance@gmail.com>
2025-02-20 14:51:21 -0800

    remove unused imports

--- src/main/java/frc/robot/subsystems/OuttakeComp.java
+++ src/main/java/frc/robot/subsystems/OuttakeComp.java
@@ -1,8 +1,6 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 
