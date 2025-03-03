6c0b2fc1f189eb9c4b5b6e32eeb46b903d0bcb6e
chem7397 <113394068+chem7397@users.noreply.github.com>
2025-03-03 09:29:34 -0800

    update gversion location

--- src/main/java/frc/robot/util/BuildData.java
+++ src/main/java/frc/robot/util/BuildData.java
@@ -4,16 +4,16 @@ package frc.robot.util;
  * Automatically generated file containing build version information.
  */
 public final class BuildData {
-  public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FRC2025";
-  public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 161;
-  public static final String GIT_SHA = "8f8db5abfe5c5d7e9fafbedcf3d171976a0ad4dd";
-  public static final String GIT_DATE = "2025-01-05 19:25:14 PST";
-  public static final String GIT_BRANCH = "update";
-  public static final String BUILD_DATE = "2025-01-06 09:02:10 PST";
-  public static final long BUILD_UNIX_TIME = 1736182930684L;
-  public static final int DIRTY = 1;
+	public static final String MAVEN_GROUP = "";
+	public static final String MAVEN_NAME = "2025RobotCode";
+	public static final String VERSION = "unspecified";
+	public static final int GIT_REVISION = 681;
+	public static final String GIT_SHA = "4219ada1bd1ccd18e2eb5207aa88383c816c0803";
+	public static final String GIT_DATE = "2025-03-03T17:19:30Z";
+	public static final String GIT_BRANCH = "New-Logger";
+	public static final String BUILD_DATE = "2025-03-03T17:28:57Z";
+	public static final long BUILD_UNIX_TIME = 1741022937425L;
+	public static final int DIRTY = 1;
 
-  private BuildData(){}
+	private BuildData(){}
 }
