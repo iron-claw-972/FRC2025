4219ada1bd1ccd18e2eb5207aa88383c816c0803
chem7397 <113394068+chem7397@users.noreply.github.com>
2025-03-03 09:19:30 -0800

    Advatnage kit started

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,13 @@ package frc.robot;
 
 import java.util.Optional;
 
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
 import au.grapplerobotics.CanBridge;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotController;
@@ -22,7 +29,7 @@ import frc.robot.constants.swerve.DriveConstants;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
     private Command autoCommand;
     private RobotContainer robotContainer;
 
@@ -40,11 +47,54 @@ public class Robot extends TimedRobot {
         //   SimGUI: Persistent Values, Preferences, RobotId, then restart Simulation
         //     changes networktables.json, networktables.json.bck (both Untracked)
         //   Uncomment the next line, set the desired RobotId, deploy, and then comment the line out
-         RobotId.setRobotId(RobotId.SwerveCompetition);
+        // RobotId.setRobotId(RobotId.SwerveCompetition);
         DriveConstants.update(RobotId.getRobotId());
         RobotController.setBrownoutVoltage(6.0);
         // obtain this robot's identity
         RobotId robotId = RobotId.getRobotId();
+
+          // Record metadata
+    // Logger.recordMetadata("ProjectName", GVersion.MAVEN_NAME);
+    // Logger.recordMetadata("BuildDate", GVersion.BUILD_DATE);
+    // Logger.recordMetadata("GitSHA", GVersion.GIT_SHA);
+    // Logger.recordMetadata("GitDate", GVersion.GIT_DATE);
+    // Logger.recordMetadata("GitBranch", GVersion.GIT_BRANCH);
+    // switch (BuildConstants.DIRTY) {
+    //   case 0:
+    //     Logger.recordMetadata("GitDirty", "All changes committed");
+    //     break;
+    //   case 1:
+    //     Logger.recordMetadata("GitDirty", "Uncomitted changes");
+    //     break;
+    //   default:
+    //     Logger.recordMetadata("GitDirty", "Unknown");
+    //     break;
+    // }
+
+    // // Set up data receivers & replay source
+    // switch (Constants.currentMode) {
+    //   case REAL:
+    //     // Running on a real robot, log to a USB stick ("/U/logs")
+    //     Logger.addDataReceiver(new WPILOGWriter());
+    //     Logger.addDataReceiver(new NT4Publisher());
+    //     break;
+
+    //   case SIM:
+    //     // Running a physics simulator, log to NT
+    //     Logger.addDataReceiver(new NT4Publisher());
+    //     break;
+
+    //   case REPLAY:
+    //     // Replaying a log, set up replay source
+    //     setUseTiming(false); // Run as fast as possible
+    //     String logPath = LogFileUtil.findReplayLog();
+    //     Logger.setReplaySource(new WPILOGReader(logPath));
+    //     Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
+    //     break;
+    // }
+
+    // Start AdvantageKit logger
+    Logger.start();
 
         // build the RobotContainer with the robot id from preferences
         robotContainer = new RobotContainer(robotId);
---
+++ src/main/java/frc/robot/frc/robot/GVersion.java
@@ -0,0 +1,19 @@
+package frc.robot.frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class GVersion {
+	public static final String MAVEN_GROUP = "";
+	public static final String MAVEN_NAME = "2025RobotCode";
+	public static final String VERSION = "unspecified";
+	public static final int GIT_REVISION = 680;
+	public static final String GIT_SHA = "48f40316a4e018d18584af3fbbe3b201f572ad5e";
+	public static final String GIT_DATE = "2025-03-03T15:21:00Z";
+	public static final String GIT_BRANCH = "New-Logger";
+	public static final String BUILD_DATE = "2025-03-03T17:13:42Z";
+	public static final long BUILD_UNIX_TIME = 1741022022604L;
+	public static final int DIRTY = 1;
+
+	private GVersion(){}
+}
