bdc3695e1248c27b03c625f92f7c58509c1d7f34
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2025-03-13 15:44:44 -0700

    Update camera positions

--- src/main/java/frc/robot/commands/gpm/OuttakeAlgae.java
+++ src/main/java/frc/robot/commands/gpm/OuttakeAlgae.java
@@ -1,9 +1,6 @@
 package frc.robot.commands.gpm;
 
-import java.util.function.BooleanSupplier;
-
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.constants.IntakeConstants;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.outtake.Outtake;
