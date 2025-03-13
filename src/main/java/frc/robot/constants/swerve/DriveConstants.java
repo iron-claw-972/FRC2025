bf539862e7dfdd662d46cb5205ff1434d36545f6
chem7397 <113394068+chem7397@users.noreply.github.com>
2025-03-12 18:56:06 -0700

    change wheel radius

--- src/main/java/frc/robot/constants/swerve/DriveConstants.java
+++ src/main/java/frc/robot/constants/swerve/DriveConstants.java
@@ -27,7 +27,7 @@ public class DriveConstants {
     public static double ROBOT_MASS = 30;
 
     /** Radius of the drive wheels [meters]. */
-    public static final double WHEEL_RADIUS = Units.inchesToMeters(2);
+    public static final double WHEEL_RADIUS = Units.inchesToMeters(1.79);
 
     public static double WHEEL_MOI = 0.000326 * ROBOT_MASS;
 
