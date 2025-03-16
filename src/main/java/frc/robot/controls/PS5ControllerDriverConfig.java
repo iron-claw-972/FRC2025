6f1f41c88cbbf7c91003f44ba16a898632c3b82b
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2025-03-16 06:52:33 -0700

    Deadband

--- src/main/java/frc/robot/controls/PS5ControllerDriverConfig.java
+++ src/main/java/frc/robot/controls/PS5ControllerDriverConfig.java
@@ -9,9 +9,9 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -251,7 +251,11 @@ public class PS5ControllerDriverConfig extends BaseDriverConfig {
             CommandScheduler.getInstance().cancelAll();
         }));
 
-        driver.get(PS5Button.MUTE).and(menu).onTrue(new RunCommand(()->getDrivetrain().alignWheels(), getDrivetrain()).withTimeout(2));
+        driver.get(PS5Button.MUTE).and(menu).onTrue(new FunctionalCommand(
+            ()->getDrivetrain().setStateDeadband(false),
+            getDrivetrain()::alignWheels,
+            interrupted->getDrivetrain().setStateDeadband(true),
+            ()->false, getDrivetrain()).withTimeout(2));
     }
 
     private void setAlignmentDirection(){
