645efc97c31b7bb5be3b42e9161ce6dcabc47267
iefomit <timofei.stem@gmail.com>
2025-03-21 16:57:10 -0700

    Delete unused import

--- src/main/java/frc/robot/controls/GameControllerDriverConfig.java
+++ src/main/java/frc/robot/controls/GameControllerDriverConfig.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
