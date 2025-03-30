47fbcabf54dce476055ef2456bf610de30c05c46
Taran Nathan <moogoesmeow123@gmail.com>
2025-03-30 15:51:21 -0700

    credential test

---
+++ src/main/java/frc/robot/commands/vision/LogVision.java
@@ -0,0 +1,32 @@
+package frc.robot.commands.vision;
+
+import java.util.function.Supplier;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.util.Vision.DetectedObject;
+
+public class LogVision extends Command {
+    private Supplier<DetectedObject> objectSupplier;
+    public LogVision(Supplier<DetectedObject> objectSupplier){
+        this.objectSupplier = objectSupplier;
+    }
+
+    @Override
+    public void execute() {
+        DetectedObject object = this.objectSupplier.get();
+        if (object != null) Logger.recordOutput("Vision/object_angle", object.getAngle() + (Math.PI/2.0));
+    }
+
+    @Override
+    public boolean runsWhenDisabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    
+}
