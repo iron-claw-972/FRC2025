9e0f4b1c64e6d604feb0088a168276ad86a0df62
maxwtan <100314265+MaxwellTTan20@users.noreply.github.com>
2025-11-19 19:26:02 -0800

    Revert "strip length"
    This reverts commit 458f913c6a26d5e1ba9df4a53f87923d89e59936.

--- src/main/java/frc/robot/subsystems/LED/LED.java
+++ src/main/java/frc/robot/subsystems/LED/LED.java
@@ -19,11 +19,6 @@ public class LED extends SubsystemBase {
     public static final int stripLength = 132;
 
     private final CANdleConfiguration config = new CANdleConfiguration();
-    config.stripType = LEDStripType.GRB;
-    config.brightnessScalar = 1.0;
-    config.vBatOutputMode = VBatOutputMode.On;
-    config.numLEDs = stripLength; 
-
     private int defenseCounter = 0;
     private int strobeCounter = 0;
 
@@ -35,8 +30,6 @@ public class LED extends SubsystemBase {
     // Constructor
     public LED() {
         this.candle = new CANdle(IdConstants.CANDLE_ID, "CANivore");
-
-        System.out.println("Max strip length = " + candle.getMaxLEDs());
 
         candle.configAllSettings(config);
 
