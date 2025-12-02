5d7097f2a6f418a3a874a90502555413cdce997d
Timofei-N <timofei.stem@gmail.com>
2025-12-01 16:18:58 -0800

    negated poses for proper object alignment

--- src/main/java/frc/robot/util/Vision/DetectedObject.java
+++ src/main/java/frc/robot/util/Vision/DetectedObject.java
@@ -309,7 +309,9 @@ public class DetectedObject {
     public double getAngle(){
         Pose2d drivePose = drive.getPose();
         // System.out.println("pose:(" + pose.getX() + ", " + pose.getY() + "); drive:(" + drivePose.getX() + ", " + drivePose.getY() + ")");
-        return Math.atan2(pose.getY()-drivePose.getY(), pose.getX()-drivePose.getX());
+        // return Math.atan2(pose.getY()-drivePose.getY(), pose.getX()-drivePose.getX());
+        return Math.atan2(-pose.getY()+drivePose.getY(), -pose.getX()+drivePose.getX());
+
     }
 
     /**
